9998b3b3eb0bb692ddfe55adee49ba18ae7dc72c
Scott Sorensen <scott.sorensen@octanner.com>
2013-01-12 21:02:42 -0700

    work on board model

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,9 +1,9 @@
 class Board
 
-  attr_reader :size
+  attr_reader :size, :grid
 
-  def initialize
-    @size = 10
+  def initialize size=10
+    @size = size
     @grid = Array.new(@size)
     @size.times do |x|
       @grid[x] = Array.new(@size)
@@ -13,17 +13,55 @@ class Board
     end
   end
 
-  def live! x, y
+  def print_cell x,y
+    if live? x,y 
+      print "X"
+    else
+      print "O"
+    end
+  end
+
+  def print_board
+    @size.times do |x|
+      print "|"
+      @size.times do |y|
+        print_cell x,y
+        print "|"
+      end
+      print "\n"
+    end
+  end
+
+  def live! x,y
     @grid[x][y] = true
     nil
   end
 
-  def dead! x, y
+  def dead! x,y
     @grid[x][y] = false
     nil
   end
 
-  def live? x, y
+  def live? x,y
     @grid[x][y]
+  end
+
+  def live_cnt x,y
+    if live? x,y
+      1
+    else
+      0
+    end
+  end
+
+  def live_neighbors x,y
+    (live_cnt x-1, y-1)
+      + (live_cnt x-1, y)
+      + (live_cnt x-1, y+1)
+      + (live_cnt x, y-1)
+      + (live_cnt x, y+1)
+      + (live_cnt x+1, y-1)
+      + (live_cnt x+1, y)
+      + (live_cnt x+1, y+1)
   end
 end
--- spec/models/board_spec.rb
+++ spec/models/board_spec.rb
@@ -25,4 +25,26 @@ describe Board do
     board.live!(2,2)
     board.live?(2,2).should be true
   end
+
+  it "can retrieve count of zero live neighbors" do
+    board = Board.new
+    board.live_neighbors(2,2).should be 0
+  end
+
+  it "can retrieve count of all live neighbors" do
+    #   0  1  2
+    # 0 x  x  x
+    # 1 x  ?  x
+    # 2 x  x  x
+    board = Board.new 3
+    3.times do |x|
+      3.times do |y|
+        board.live! x,y
+      end
+    end
+
+    board.print_board
+    board.live_neighbors(1,1).should eq 9
+
+  end
 end
